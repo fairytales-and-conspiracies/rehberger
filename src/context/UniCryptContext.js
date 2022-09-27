cc0451dae0b781028d269d2f6bd54c7222b11ff5
Marko Stojanovic <marko.stojanovic.bss@gmail.com>
2022-09-27 21:02:43 +0200

    Serialize price fetching requests

--- src/context/UniCryptContext.js
+++ src/context/UniCryptContext.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
-import { createContext, useCallback, useState } from 'react';
+import { createContext, useCallback, useEffect, useState } from 'react';
 
 const UniCryptContext = createContext();
 
 export function UniCryptProvider({ children }) {
   const [ethToEurRate, setEthToEurRate] = useState(undefined);
+  const [rateFetchingRequested, requestRateFetching] = useState(false);
 
   const fetchEthToEurRate = useCallback(async () => {
     const { data } = await axios.post('/api/unicrypt-rates', {
@@ -21,13 +22,22 @@ export function UniCryptProvider({ children }) {
     return amount;
   }, []);
 
+  useEffect(() => {
+    if (rateFetchingRequested) {
+      fetchEthToEurRate();
+      setTimeout(() => {
+        requestRateFetching(false);
+      }, 10000);
+    }
+  }, [fetchEthToEurRate, rateFetchingRequested]);
+
   const getEthToEurRate = useCallback(() => {
     if (ethToEurRate === undefined) {
-      fetchEthToEurRate();
+      requestRateFetching(true);
     }
 
     return ethToEurRate;
-  }, [ethToEurRate, fetchEthToEurRate]);
+  }, [ethToEurRate]);
 
   return (
     <UniCryptContext.Provider
