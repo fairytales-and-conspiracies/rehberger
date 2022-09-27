13e8e3e904b29337c47e2efb1b9cc8e5d69a3bb6
Marko Stojanovic <marko.stojanovic.bss@gmail.com>
2022-09-27 22:21:51 +0200

    Stripe cancel - Go to shopping cart instead of cancel url

--- src/pages/api/orders.js
+++ src/pages/api/orders.js
@@ -140,7 +140,7 @@ const createOrder = async (req) => {
 
 const checkout = async (confirmationKey, items, priceInCents) => {
   const session = await stripe.checkout.sessions.create({
-    cancel_url: `${SERVER_URL}/cancel`,
+    cancel_url: `${SERVER_URL}/shopping-cart`,
     client_reference_id: confirmationKey,
     line_items: items.map((item) => ({
       price_data: {
