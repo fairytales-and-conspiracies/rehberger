c5e928911b18e6541078ef2da37da92e8ef17261
Marko Stojanovic <marko.stojanovic.bss@gmail.com>
2022-09-29 01:34:25 +0200

    Change 3

--- src/pages/api/orders2.js
+++ src/pages/api/orders2.js
@@ -1,14 +1,123 @@
+import crypto from 'crypto';
+
 import dbConnect from '@/lib/dbConnect';
+import Frame from '@/models/Frame';
+import Order from '@/models/Order';
+import TransactionStatus from '@/static-data/transaction-status';
+import { ethToEur } from '@/utils/conversion';
+import { padZeroes } from '@/utils/string';
+import calculateVat from '@/utils/vat';
+
+const NFT_PRICE_ETH = parseFloat(process.env.NEXT_PUBLIC_NFT_PRICE_ETH);
+
+export const orderFramesMongoFilter = (frames) => {
+  const filter = { _id: { $in: [] } };
+  frames.forEach(async (frame) => {
+    // eslint-disable-next-line no-underscore-dangle
+    const id = frame._id.toString();
+    // eslint-disable-next-line no-underscore-dangle
+    filter._id.$in.push(id);
+  });
+  return filter;
+};
+
+export const getNextOrderNumber = async () => {
+  const orderArr = await Order.find().sort('-orderNumber').limit(1);
+  const order = orderArr[0];
+  if (!order || !order.orderNumber) {
+    return 1;
+  }
+
+  return order.orderNumber + 1;
+};
+
+const fillOutRestOfOrderData = (customer, frames, ethToEurRate) => {
+  const orderCreatedTimestamp = Date.now();
+  const quantity = frames.length;
+
+  const framePriceETH = NFT_PRICE_ETH;
+  const framePriceEUR = ethToEur(NFT_PRICE_ETH, ethToEurRate);
+
+  // Calculate the price so that if sales tax exists, it is
+  // subtracted from the total price. In other words, the buyer
+  // does not pay the tax, so the price per NFT is always the same,
+  // as defined.
+  const totalPriceETH = framePriceETH * quantity;
+  const totalPriceEUR = framePriceEUR * quantity;
+
+  const { country, vatNo } = customer;
+  const vat = calculateVat(country, vatNo);
+
+  const netPriceETH = parseFloat((totalPriceETH / (1.0 + vat)).toFixed(2));
+  const netPriceEUR = parseFloat((totalPriceEUR / (1.0 + vat)).toFixed(2));
+  return {
+    orderCreatedTimestamp,
+    quantity,
+    framePriceETH,
+    netPriceETH,
+    totalPriceETH,
+    framePriceEUR,
+    netPriceEUR,
+    totalPriceEUR,
+    vat,
+  };
+};
+
+const createOrder = async (req) => {
+  const { customer, frames, paymentMethod, ethToEurRate } = req.body;
+
+  const isWalletPayment = paymentMethod === 'WALLET';
+  const restOfOrderData = fillOutRestOfOrderData(
+    customer,
+    frames,
+    ethToEurRate
+  );
+  const body = {
+    ...req.body,
+    frames,
+    ...restOfOrderData,
+    toBeClaimed: !isWalletPayment,
+  };
+
+  if (isWalletPayment) {
+    body.transactionStatus = TransactionStatus.SUCCESSFUL;
+    body.transactionSuccessfulTimestamp = body.orderCreatedTimestamp;
+    body.orderNumber = await getNextOrderNumber();
+    body.invoiceNumber = `NFT${padZeroes(body.orderNumber, 6)}`;
+
+    const filter = orderFramesMongoFilter(frames);
+    await Frame.updateMany(filter, { sold: true });
+  } else {
+    body.claimed = false;
+    body.confirmationKey = crypto.randomBytes(16).toString('hex');
+    body.transactionStatus = TransactionStatus.PENDING;
+  }
+
+  let order;
+  try {
+    order = await Order.create(body);
+  } catch (err) {
+    order = null;
+  }
+  return order;
+};
 
 const handler = async (req, res) => {
+  console.log('SIGURICA', req);
+
   const { method } = req;
 
   await dbConnect();
 
   switch (method) {
-    case 'POST':
+    case 'POST': {
+      // const { frames, paymentMethod, transactionHash } = req.body;
+      const order = await createOrder(req);
+      console.log('ORDER', order);
+
       res.status(201).json({ success: true, data: 'lala' });
       break;
+    }
     default:
       res.status(400).json({ success: false, err: `Error 3` });
       break;
