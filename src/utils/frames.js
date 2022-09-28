6f669d06025347b2991c3f4cd7e6f2aafda41cb9
fairytales-and-conspiracies <f.and.c@yandex.com>
2022-09-28 17:41:47 +0200

    add some more changes

--- src/utils/frames.js
+++ src/utils/frames.js
@@ -3,7 +3,7 @@ import { padZeroes } from '@/utils/string';
 
 export const getFrameName = (frame) => {
   const padding = videos[frame.video].frames > 999 ? 4 : 3;
-  return `${videos[frame.video].title} ${padZeroes(frame.frame, padding)}`;
+  return `${videos[frame.video].title} #${padZeroes(frame.frame, padding)}`;
 };
 export const getFrameFileName = (frame) => {
   const padding = videos[frame.video].frames > 999 ? 4 : 3;
