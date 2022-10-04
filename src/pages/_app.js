8dc0897444f7092f351ca0b7e85afd47f49ad338
Marko Stojanovic <marko.stojanovic.bss@gmail.com>
2022-10-04 17:57:03 +0200

    When changing page instant scroll to top

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -9,12 +9,29 @@ import { ShoppingCartProvider } from '@/context/ShoppingCartContext';
 import { UniCryptProvider } from '@/context/UniCryptContext';
 import { Web3Provider } from '@/context/Web3Context';
 import '@/styles/globals.scss';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 function getLibrary(provider) {
   return new Web3(provider);
 }
 
 function MyApp({ Component, pageProps }) {
+  const { pathname, asPath } = useRouter();
+
+  useEffect(() => {
+    const sectionId = asPath.split(`${pathname}#`)[1];
+
+    // some browsers (like safari) may require a timeout to delay calling this function after a page has loaded; otherwise, it may not update the position
+    setTimeout(() => {
+      window.scrollTo({
+        top: sectionId ? document.getElementById(sectionId).offsetTop : 0,
+        left: 0,
+        behavior: 'instant',
+      });
+    }, 0);
+  }, [asPath, pathname]);
+
   return (
     <>
       <Head>
