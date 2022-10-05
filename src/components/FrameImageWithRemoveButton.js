e1db3318ea71da1a308fcb9f7c29d3e6d2392ae5
fairytales-and-conspiracies <f.and.c@yandex.com>
2022-10-06 00:07:25 +0200

    feat: selection preview images now load faster and show frame number beneath

--- src/components/FrameImageWithRemoveButton.js
+++ src/components/FrameImageWithRemoveButton.js
@@ -1,7 +1,8 @@
 import Image from 'next/image';
+import { useState } from 'react';
 
 import VideoData from '@/static-data/videos';
-import { getFrameFileName } from '@/utils/frames';
+import { getFrameFileName, getPaddedFrameNumber } from '@/utils/frames';
 
 const NFT_IMAGE_URL = process.env.NEXT_PUBLIC_NFT_IMAGE_URL;
 const NFT_IMAGE_EXTENSION = process.env.NEXT_PUBLIC_NFT_IMAGE_EXTENSION;
@@ -11,6 +12,8 @@ export default function FrameImageWithRemoveButton({
   isInShoppingCart,
   onRemoveClick,
 }) {
+  const [imageLoaded, setImageLoaded] = useState(false);
+
   let imageSrc;
   if (frame) {
     imageSrc =
@@ -20,38 +23,52 @@ export default function FrameImageWithRemoveButton({
   }
 
   return (
-    <span
-      className={`frame-image ${
-        isInShoppingCart ? 'frame-image--in-shopping-cart' : ''
-      }`}
-    >
-      <Image
-        alt={`${VideoData[frame.video].title} frame ${frame.frame}`}
-        height="100%"
-        layout="responsive"
-        src={imageSrc || '/img/frames/0'}
-        width="75%"
-      />
-      <span className="frame-image__remove-container">
-        <Image
-          alt={`Remove frame ${frame.frame}`}
-          className="frame-image__remove-icon"
-          height="18px"
-          onClick={() => onRemoveClick(frame)}
-          src="/img/icons/trash.svg"
-          width="14px"
+    <span className="frame-image">
+      <span
+        className={`frame-image__main ${
+          isInShoppingCart ? 'frame-image__main--in-shopping-cart' : ''
+        }`}
+      >
+        <img
+          alt={`${VideoData[frame.video].title} frame ${frame.frame}`}
+          className="frame-image__img"
+          height="100%"
+          loading="eager"
+          onLoad={() => setImageLoaded(true)}
+          src={imageSrc || '/img/frames/0'}
+          width="75%"
         />
+        <span className="frame-image__remove-container">
+          <Image
+            alt={`Remove frame ${frame.frame}`}
+            className="frame-image__remove-icon"
+            height="18px"
+            onClick={() => onRemoveClick(frame)}
+            src="/img/icons/trash.svg"
+            width="14px"
+          />
+        </span>
+        <span className="frame-image__remove-container--smaller-devices">
+          <Image
+            alt={`Remove frame ${frame.frame}`}
+            className="frame-image__remove-icon"
+            height="30px"
+            onClick={() => onRemoveClick(frame)}
+            src="/img/icons/trash.svg"
+            width="24px"
+          />
+        </span>
       </span>
-      <span className="frame-image__remove-container--smaller-devices">
-        <Image
-          alt={`Remove frame ${frame.frame}`}
-          className="frame-image__remove-icon"
-          height="30px"
-          onClick={() => onRemoveClick(frame)}
-          src="/img/icons/trash.svg"
-          width="24px"
-        />
-      </span>
+      {!isInShoppingCart && (
+        <div
+          className={`frame-image__info ${
+            !imageLoaded ? 'frame-image__info--invisible' : ''
+          }`}
+        >
+          <span className="frame-image__title">Frame</span>
+          {getPaddedFrameNumber(frame)}
+        </div>
+      )}
     </span>
   );
 }
