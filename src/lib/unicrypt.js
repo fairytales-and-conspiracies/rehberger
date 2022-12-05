a89ad0d93f49e5129cad2609a01176dbb120a25b
fairytales-and-conspiracies <f.and.c@yandex.com>
2022-12-05 23:48:00 +0100

    feat: use Coinbase API instead of UniCrypt API for getting ETH to EUR rates

--- src/lib/unicrypt.js
+++ src/lib/unicrypt.js
@@ -1,6 +1,9 @@
 import { logError } from '@/utils/log';
 import axios from 'axios';
 
+// This has been changed to Coinbase API, but we kept
+// the UniCrypt name to avoid unnecessary refactoring
+
 const uniCryptApi = axios.create({
   baseURL: process.env.NEXT_PUBLIC_UNICRYPT_API,
 });
@@ -12,31 +15,33 @@ async function uniCryptConvert(req) {
       destination_currency: destinationCurrency,
     } = req;
 
-    const path = `/markets`;
-    const ret = await uniCryptApi.get(path);
+    const path = `/exchange-rates?currency=${sourceCurrency}`;
+    const { data } = await uniCryptApi.get(path);
+    const rate = +data.data.rates[destinationCurrency];
+    return rate;
 
-    const firstPairName = `${sourceCurrency}/USD`;
-    const secondPairName = `${destinationCurrency}/USD`;
-    const resultPairName = `${sourceCurrency}/${destinationCurrency}`;
+    // const firstPairName = `${sourceCurrency}/USD`;
+    // const secondPairName = `${destinationCurrency}/USD`;
+    // const resultPairName = `${sourceCurrency}/${destinationCurrency}`;
 
-    const firstPairPrice = ret.data.find(
-      (pair) => pair.name === firstPairName
-    )?.price;
-    const secondPairPrice = ret.data.find(
-      (pair) => pair.name === secondPairName
-    )?.price;
-    const resultPairPrice =
-      firstPairPrice && secondPairPrice
-        ? firstPairPrice / secondPairPrice
-        : undefined;
+    // const firstPairPrice = ret.data.find(
+    //   (pair) => pair.name === firstPairName
+    // )?.price;
+    // const secondPairPrice = ret.data.find(
+    //   (pair) => pair.name === secondPairName
+    // )?.price;
+    // const resultPairPrice =
+    //   firstPairPrice && secondPairPrice
+    //     ? firstPairPrice / secondPairPrice
+    //     : undefined;
 
-    console.log('PRICES', {
-      [firstPairName]: firstPairPrice,
-      [secondPairName]: secondPairPrice,
-      [resultPairName]: resultPairPrice,
-    });
+    // console.log('PRICES', {
+    //   [firstPairName]: firstPairPrice,
+    //   [secondPairName]: secondPairPrice,
+    //   [resultPairName]: resultPairPrice,
+    // });
 
-    return resultPairPrice;
+    // return resultPairPrice;
   } catch (e) {
     logError(e);
     throw e;
